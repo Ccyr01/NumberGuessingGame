073a0d1f69263a0c115456ce267632bf83f1a1b7
Ccyr01 <82624847+Ccyr01@users.noreply.github.com>
2023-01-27 00:44:10 -0700

    first implementation

---
+++ NumberGuessingGame.py
@@ -0,0 +1,50 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+"""
+
+import random
+
+
+def start_game():
+    """Psuedo-code Hints
+    
+    When the program starts, we want to:
+    ------------------------------------
+    1. Display an intro/welcome message to the player.
+    2. Store a random number as the answer/solution.
+    3. Continuously prompt the player for a guess.
+      a. If the guess greater than the solution, display to the player "It's lower".
+      b. If the guess is less than the solution, display to the player "It's higher".
+    
+    4. Once the guess is correct, stop looping, inform the user they "Got it"
+         and show how many attempts it took them to get the correct number.
+    5. Let the player know the game is ending, or something that indicates the game is over.
+    
+    ( You can add more features/enhancements if you'd like to. )
+    """
+    # Display intro message
+    count = 1;
+    print("Welcome to the Number Guessing Game!!")
+    numberToGuess = random.randint(0,10)
+    userGuess = int(input("pick a number."))  
+          
+    while userGuess != numberToGuess:
+        if userGuess < numberToGuess:
+            print("To low")
+            count+=1
+        else:
+            print("To high")
+            count+=1
+        userGuess = int(input("pick a number."))  
+
+
+    print("Got it!! It took about {}".format(count)+" times")
+    
+
+
+
+
+# Kick off the program by calling the start_game function.
+start_game()
