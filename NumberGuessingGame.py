f589b2dc66f1972e05281f1af2af60013b43fe4c
Ccyr01 <82624847+Ccyr01@users.noreply.github.com>
2023-01-27 11:04:37 -0700

    checking for exceptions other bugs

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,4 +1,5 @@
 """
+Christian C. 1/27/2023
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
@@ -28,22 +29,32 @@ def start_game():
     count = 1;
     print("Welcome to the Number Guessing Game!!")
     numberToGuess = random.randint(0,10)
-    userGuess = int(input("pick a number."))  
-          
-    while userGuess != numberToGuess:
-        if userGuess < numberToGuess:
-            print("To low")
-            count+=1
+    #Checking if user entered value is valid
+    try:
+        userGuess = int(input("Pick a number between 0 and 10 inclusive.\n"))  
+    #Non int entered or invalid one
+    except ValueError:
+        print("Please enter a valid value")
+    #Have a valid number at this point.
+    #Proceed with game
+    else:      
+        while userGuess != numberToGuess:
+            if userGuess < numberToGuess:
+                print("To low")
+                count+=1
+            else:
+                print("To high")
+                count+=1
+            userGuess = int(input("pick a number."))  
+
+        if count < 2:
+            print("Wow!! Can't believe you guessed it in {}".format(count)+ " try. You've got good luck")
+        elif count <= 3:
+            print("Nice. Guessed it in {}".format(count) + " tries. That's good luck.")
         else:
-            print("To high")
-            count+=1
-        userGuess = int(input("pick a number."))  
-
-
-    print("Got it!! It took about {}".format(count)+" times")
-    
-
-
+            print("Got it! It took about {}".format(count)+" times.")
+    #farewell
+    print("Give it another shot if you want to test your luck!")
 
 
 # Kick off the program by calling the start_game function.
